51839e5bdf9ef136ba9323e3fc6c60faeb76887a
foo2810 <anime_117@yahoo.co.jp>
2019-03-18 22:22:15 +0900

    pe_parser package

---
+++ pe_parser/parse_pe.py
@@ -0,0 +1,185 @@
+# PE Parser
+
+##########################################################################
+# c.f.                                                                   #
+# https://en.wikibooks.org/wiki/X86_Disassembly/Windows_Executable_Files #
+# http://hp.vector.co.jp/authors/VA050396/tech_06.html                   #
+#                                                                        #
+##########################################################################
+
+# -*- coding: utf-8 -*-
+
+import os
+from array import array
+from mmap import *
+
+from pe_parser._peHeader import *
+from pe_parser._importTable import *
+from pe_parser._exportTable import *
+from pe_parser._relocationTable import *
+from pe_parser._binary import *
+
+
+class PEParser:
+	def __init__(self, pefile):
+		self.bData = None
+		self.peHeader = None
+		with open(pefile, "rb") as st:
+			self.bData = st.read()
+		
+		self.importTable = None
+		self.exportTable = None
+		self.relocationTable = None
+		
+		self.filename = os.path.abspath(pefile)
+	
+	def parse(self):
+		try:
+			self.peHeader = PEHeader(self.bData)
+			self.__loadToMemory(False)
+		except:
+			return False
+		
+		return True
+	
+	def getMSDosHeader(self):
+		return self.peHeader.msDosHeader
+		
+	def getNTHeader(self):
+		return self.peHeader.ntHeader
+		
+	def getFileHeader(self):
+		return self.peHeader.fileHeader
+	
+	def getOptionalHeader(self):
+		return self.peHeader.optionalHeader
+	
+	def getDataDirectory(self):
+		return self.peHeader.dataDirectory
+	
+	def getSectionTable(self):
+		return self.peHeader.sectionTable
+		
+	def getImportTable(self):
+		if self.importTable is not None:
+			return self.importTable
+	
+		importTableVRva = self.peHeader.optionalHeader.DataDirectory[1].VirtualAddress
+		importTableSize = self.peHeader.optionalHeader.DataDirectory[1].Size
+		if self.peHeader.optionalHeader.Magic == b"\x0b\x01":
+			magic = 32
+		elif self.peHeader.optionalHeader.Magic == b"\x0b\x02":
+			magic = 64
+		else:
+			raise ROMImage("in dumpImportTable")
+			
+		self.importTable = ImportTable(self.mapData, importTableVRva, importTableSize, magic)
+		return self.importTable
+	
+	def getExportTable(self):
+		if self.exportTable is not None:
+			return self.exportTable
+		
+		exportTableVRva = self.peHeader.optionalHeader.DataDirectory[0].VirtualAddress
+		exportTableSize = self.peHeader.optionalHeader.DataDirectory[0].Size
+		self.exportTable = ExportTable(self.mapData, exportTableVRva, exportTableSize)
+		
+		return self.exportTable
+	
+	def getRelocationTable(self):
+		if self.relocationTable is not None:
+			return self.relocationTable
+		
+		relocationTableVRva = self.peHeader.optionalHeader.DataDirectory[5].VirtualAddress
+		relocationTableSize = self.peHeader.optionalHeader.DataDirectory[5].Size
+		self.relocationTable = RelocationTable(self.mapData, relocationTableVRva, relocationTableSize)
+		
+		return self.relocationTable
+		
+	def dumpImportTable(self, flg=1):
+		self.importTable = self.getImportTable()
+		self.importTable.printAll(flg)
+	
+	def dumpExportTable(self):
+		self.exportTable = self.getExportTable()
+		self.exportTable.printAll()
+	
+	def dumpRelocationTable(self):
+		self.relocationTable = self.getRelocationTable()
+		self.relocationTable.printAll()
+	
+	def __loadToMemory(self, printOn=False):
+		alignment = self.peHeader.ntHeader.OptionalHeader.SectionAlignment
+		baseAddr = self.peHeader.ntHeader.OptionalHeader.ImageBase
+		mapSize = self.peHeader.ntHeader.OptionalHeader.SizeOfImage
+		#entryPoint = self.peHeader.ntHeader.OptionalHeader.AddressOfEntryPoint
+		
+		if printOn:
+			print("[Caution] loadToMemory is not COMPLETED!!!!!!!!!!\n")
+			print("[LOAD PE FORMAT FILE]")
+			print("BaseAddress: ", hex(baseAddr))
+			print("Alignment: ", hex(alignment))
+			#print("EntryPoint: ", hex(entryPoint))
+		
+		
+		self.mapData = mmap(-1, mapSize, None, ACCESS_WRITE)
+		
+		if printOn:
+			print("[Sections]")
+		for section in self.peHeader.sectionTable:
+			if not section.isAlloced:
+				continue
+			
+			name = section.name
+			vRva = section.VirtualAddress
+			size = section.SizeOfRawData
+			
+			if printOn:
+				print("\tName: ", name)
+				print("\tSize: ", hex(size))
+				print("\tVirtualAddress(RVA): ", hex(vRva))
+			
+			
+			# Padding (alignment)
+			"""
+			if self.mapData.tell() % alignment != 0:
+				cnt = alignment - (self.mapData.tell() % alignment)
+				for i in range(cnt):
+					self.mapData.write_byte(0)
+			"""
+			
+			curPosB = self.mapData.tell()
+			if vRva > curPosB:
+				self.mapData.write(b"\x00" * (vRva - curPosB))
+			
+			if printOn:
+				curPosA = self.mapData.tell()
+				print("\tHead Position of MemoryMap: ", hex(curPosA))
+			
+			rawData = section.getRawData()
+			res = self.mapData.write(rawData)
+			self.mapData.flush()
+			
+			if printOn:
+				print("\tRes from write(): ", hex(res))
+				print("")
+		
+		#self.isLoaded = True
+		self.mapData.seek(0)
+	
+	def dump(self, output="dump.bin"):
+		dumpData = self.mapData.read()
+		self.mapData.seek(0)
+			
+		if output != "":
+			st = open(output, "wb")
+			
+			st.write(dumpData)
+			st.flush()
+			st.close()
+		
+		return dumpData
+	
+	def printAll(self):
+		self.peHeader.printAll()
+
---
+++ pe_parser/readpe.py
@@ -0,0 +1,191 @@
+# PE Reader
+
+from pe_parser.parse_pe import *
+
+class PEReader:
+	def __init__(self, pefile):
+		self.filename = os.path.abspath(pefile)
+		
+		if not os.path.exists(pefile):
+			raise FileNotFoundError
+		
+		self.parser = PEParser(pefile)
+	
+	def parse(self):
+		if self.parser.parse():
+			return True
+		else:
+			return False
+	
+	## MSDOS Header
+	
+	def getMSDosHeader(self):
+		msdosHeader = self.parser.getMSDosHeader()
+		
+		d = {
+			"e_magic": msdosHeader.e_magic,
+			"e_cblp": msdosHeader.e_cblp,
+			"e_cp": msdosHeader.e_cp,
+			"e_crlc": msdosHeader.e_crlc,
+			"e_cparhdr": msdosHeader.e_cparhdr,
+			"e_minalloc": msdosHeader.e_minalloc,
+			"e_ss": msdosHeader.e_ss,
+			"e_sp": msdosHeader.e_sp,
+			"e_csum": msdosHeader.e_csum,
+			"e_ip": msdosHeader.e_ip,
+			"e_cs": msdosHeader.e_cs,
+			"e_lfarlc": msdosHeader.e_lfarlc,
+			"e_ovno": msdosHeader.e_ovno,
+			"e_res": msdosHeader.e_res,
+			"e_oemid": msdosHeader.e_oemid,
+			"e_oeminfo": msdosHeader.e_oeminfo,
+			"e_res2": msdosHeader.e_res2,
+			"e_lfanew": msdosHeader.e_lfanew,
+		}
+		
+		return d
+	
+	## NT Header
+	
+	def getNTHeader(self):
+		ntHeader = self.parser.getNTHeader()
+		d = {
+			"Signature": ntHeader.Signature,
+		}
+		
+		return d
+	
+	## File Header
+	
+	def getFileHeader(self):
+		fileHeader = self.parser.getFileHeader()
+		
+		d = {
+			"Machine": fileHeader.Machine,
+			"NumberOfSections": fileHeader.NumberOfSections,
+			"TimeDataStamp": fileHeader.TimeDataStamp,
+			"PointerToSymbolTable": fileHeader.PointerToSymbolTable,
+			"NumberOfSymbols": fileHeader.NumberOfSymbols,
+			"SizeOfOptionalHeader": fileHeader.SizeOfOptionalHeader,
+			"Characteristics": hex(fileHeader.Characteristics),
+		}
+		
+		return d
+		
+	## Optional Header
+	
+	def getOptionalHeader(self):
+		optHeader = self.parser.getOptionalHeader()
+		
+		d = {
+			"Magic": optHeader.Magic, 
+			"MajorLinkerVersion": optHeader.MajorLinkerVersion,
+			"MinorLinkerVersion": optHeader.MinorLinkerVersion,
+			"SizeOfCode": optHeader.SizeOfCode,
+			"SizeOfInitializedData": optHeader.SizeOfInitializedData,
+			"SizeOfUninitializedData": optHeader.SizeOfUninitializedData,
+			"AddressOfEntryPoint": optHeader.AddressOfEntryPoint,
+			"BaseOfCode": optHeader.BaseOfCode,
+			"BaseOfData": optHeader.BaseOfData,
+			
+			"ImageBase": optHeader.ImageBase, 
+			"SectionAlignment": optHeader.SectionAlignment,
+			"FileAlignment": optHeader.FileAlignment,
+			"MajorOperatingSystemVersion": optHeader.MajorOperatingSystemVersion,
+			"MinorOperatingSystemVersion": optHeader.MinorOperatingSystemVersion,
+			"MajorImageVersion": optHeader.MajorImageVersion,
+			"MinorImageVersion": optHeader.MinorImageVersion,
+			"MajorSubsystemVersion": optHeader.MajorSubsystemVersion,
+			"MinorSubsystemVersion": optHeader.MinorSubsystemVersion,
+			"Win32VersionValue": optHeader.Win32VersionValue,
+			"SizeOfImage": optHeader.SizeOfImage,
+			"SizeOfHeaders": optHeader.SizeOfHeaders,
+			"CheckSum": optHeader.CheckSum,
+			"Subsystem": optHeader.Subsystem,
+			"DllCharacteristics": optHeader.DllCharacteristics,
+			"SizeOfStackReserve": optHeader.SizeOfStackReserve,
+			"SizeOfStackCommit": optHeader.SizeOfStackCommit,
+			"SizeOfHeapReserve": optHeader.SizeOfHeapReserve,
+			"SizeOfHeapCommit": optHeader.SizeOfHeapCommit,
+			"LoaderFlags": optHeader.LoaderFlags,
+			"NumberOfRvaAndSizes": optHeader.NumberOfRvaAndSizes,
+			"DataDirectory": "DATADIRECTORY_DUMMY",
+		}
+		
+		return d
+	
+	## About Import Table
+	
+	def getImportFunctionList(self, dllName):
+		importTable = self.parser.getImportTable()
+		for i in importTable:
+			if i.Name.lower() == dllName.lower():
+				return list(map(lambda x: x.AddressOfData.Name, i))
+		
+		return None
+					
+	
+	def getImportDllList(self):
+		importTable = self.parser.getImportTable()
+		return list(map(lambda x: x.Name, importTable))
+	
+	def getImportDllInfo(self, dllName):
+		importTable = self.parser.getImportTable()
+		for i in importTable:
+			if i.Name.lower() == dllName.lower():
+				dllInfo = dict()
+				dllInfo["Union"] = i.Union
+				dllInfo["TimeDataStamp"] = i.TimeDataStamp
+				dllInfo["ForwarderChain"] = i.ForwarderChain
+				dllInfo["NameRVA"] = i.nameRVA
+				dllInfo["Name"] = i.Name
+				dllInfo["NumberOfEntry"] = i.numberOfEntry
+				return dllInfo
+		
+		return None
+	
+	def getImportFunctionInfo(self, func, dllName=None):
+		importTable = self.parser.getImportTable()
+		for i in importTable:
+			if dllName is not None:
+				if i.Name.lower() != dllName.lower():
+					continue
+			for f in i:
+				if f.AddressOfData.Name.lower() == func.lower():
+					fInfo = dict()
+					fInfo["Name"] = f.AddressOfData.Name
+					fInfo["Rva"] = byteToIntLE(f.Union)
+					return fInfo
+		return None
+	
+	
+	## About Export Table
+	
+	def getExportFuncNameList(self):
+		exportTable = self.parser.getExportTable()
+		
+		return list(map(lambda x: x[1], exportTable))
+
+	def getExportFuncAddr(self, funcName):
+		exportTable = self.parser.getExportTable()
+		
+		for o, n in exportTable:
+			if funcName.lower() == n.lower():
+				return exportTable.exportAddressTable[o]
+		return None
+	
+	def isExported(self, func):
+		exportTable = self.parser.getExportTable()
+		return exportTable.isExported(self.getExportFuncAddr(func))
+	
+	## About Relocation Table
+	
+	def getBaseRelocationList(self):
+		pass
+	
+	def getRelocationEntryList(self, base):
+		pass
+	
+	def getRelocationInfo(self, base, ord):
+		pass
+	
